853d88d0670179f2de1bb146b7cf85d423bf24b5
owwwl666 <zaharsalam228@gmail.com>
2023-09-07 04:10:02 +1000

    Update views.py
    replaced get with get_object_or_404.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from blog.models import Post, Tag
 from django.db.models import Count
+from django.shortcuts import get_object_or_404
 
 
 def serialize_post(post):
@@ -45,7 +46,7 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.annotate(likes_count=Count('likes')).get(slug=slug)
+    post = get_object_or_404(Post.objects.annotate(likes_count=Count('likes')), slug=slug)
 
     comments = post.comments.select_related('author').all()
     serialized_comments = []
@@ -86,7 +87,7 @@ def post_detail(request, slug):
 
 
 def tag_filter(request, tag_title):
-    tag = Tag.objects.get(title=tag_title)
+    tag = get_object_or_404(Tag, title=tag_title)
 
     most_popular_tags = Tag.objects.popular()[:5]
 
